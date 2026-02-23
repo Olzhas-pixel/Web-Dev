19597963c875f39eca9cad9b5673003130e2b161
Olzhas <o_kuzembayev@kbtu.kz>
2026-02-23 15:22:13 +0500

    ff

---
+++ lab4/task2/online-store/src/app/data/products.ts
@@ -0,0 +1,34 @@
+import { Product } from "../models/product.model";
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: "Iphone 15 pro",
+    description: "Apple iPhone 15 Pro features a powerful A17 Pro chip and a titanium design.",
+    price: 900000,
+    rating: 4.7,
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hc6/h71/86302551736350.jpg?format=gallery-medium",
+    images: ["https://resources.cdn-kaspi.kz/img/m/p/h4d/hf0/86319889678366.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/hb3/ha6/86319889711134.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/ha6/h49/86319889743902.jpg?format=gallery-medium"],
+    link: "https://kaspi.kz/shop/p/apple-iphone-15-pro-256gb-sinii-113138331/?c=750000000"
+  },
+  {
+    id: 2,
+    name: "Iphone 15 pro",
+    description: "Apple iPhone 15 Pro features a powerful A17 Pro chip and a titanium design.",
+    price: 900000,
+    rating: 4.7,
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hc6/h71/86302551736350.jpg?format=gallery-medium",
+    images: ["https://resources.cdn-kaspi.kz/img/m/p/h4d/hf0/86319889678366.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/hb3/ha6/86319889711134.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/ha6/h49/86319889743902.jpg?format=gallery-medium"],
+    link: "https://kaspi.kz/shop/p/apple-iphone-15-pro-256gb-sinii-113138331/?c=750000000"
+  },
+  {
+    id: 3,
+    name: "Iphone 15 pro",
+    description: "Apple iPhone 15 Pro features a powerful A17 Pro chip and a titanium design.",
+    price: 900000,
+    rating: 4.7,
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hc6/h71/86302551736350.jpg?format=gallery-medium",
+    images: ["https://resources.cdn-kaspi.kz/img/m/p/h4d/hf0/86319889678366.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/hb3/ha6/86319889711134.jpg?format=gallery-medium", "https://resources.cdn-kaspi.kz/img/m/p/ha6/h49/86319889743902.jpg?format=gallery-medium"],
+    link: "https://kaspi.kz/shop/p/apple-iphone-15-pro-256gb-sinii-113138331/?c=750000000"
+  }
+];
